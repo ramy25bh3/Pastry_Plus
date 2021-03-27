5a03d49a9975900b9f9cd6b295a38a5aa0c22b2a
Cyrine-afk <cyrine.trabelsi@esprit.tn>
2021-03-27 01:41:00 +0100

    Cyrine 7th commit

--- crud/crud/fournisseur.cpp
+++ crud/crud/fournisseur.cpp
@@ -12,7 +12,7 @@ fournisseur::fournisseur()
 
 }
 
-fournisseur::fournisseur(int id_f1, QString nom_soc1, int tel_f1, QString email_f1, QDateTime date_add_f1, QString specialite_f1, QString adresse_f1)
++fournisseur::fournisseur(int id_f1, QString nom_soc1, int tel_f1, QString email_f1, QDateTime date_add_f1, QString specialite_f1, QString adresse_f1)
 {
 
     id_f=id_f1;
